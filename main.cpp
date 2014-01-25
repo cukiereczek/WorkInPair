3670b809cb446556bd1fec6215b9a6444ad3b163
cukiereczek <rewarien@gmail.com>
2014-01-25 17:47:27 +0100

    dodanie funkcji utils

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "generator.h"
+#include "utils.h"
 
 int main(int argc, char **argv)
 {
 	generator(5);
+	int t[] = { 1, 2, 3 };
+	utils( t, 3);
 	std::cin.get();
 	return 0;
 }
---
+++ utils.h
@@ -0,0 +1,7 @@
+void utils(int i[], int n){
+	int k = i[0];
+	for (int t = 0; t < n; t++){
+		k = i[t];
+		std::cout << "\n" << k;
+	}
+}
